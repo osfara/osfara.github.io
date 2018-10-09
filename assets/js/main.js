f4c3358270a02b7b54414090b2261fbe50dc001c
adair <yadaxim@gmail.com>
2018-10-09 11:27:04 +0200

    menu with no content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,64 +1,90 @@
 /*
-	Dopetrope by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+    Dopetrope by HTML5 UP
+    html5up.net | @ajlkn
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
 
-	var	$window = $(window),
-		$body = $('body');
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+ // var app = new Vue({
+ //     el: '#page-wrapper',
+ //     data: {
+ //             message: 'Hello Vue!',
+ //             menu: []
+ //     }
+ //     })
 
-	// Dropdowns.
-		$('#nav > ul').dropotron({
-			mode: 'fade',
-			noOpenerFade: true,
-			alignment: 'center'
-		});
+    $('#nav').load('/partials/nav.html').hide().fadeIn('slow', function(){
+        // Dropdowns.
+        console.log('drop')
+        $('#nav > ul').dropotron({
+            mode: 'fade',
+            noOpenerFade: true,
+            alignment: 'center'
+        });
+    });
 
-	// Nav.
+    $('#footer').load('/partials/footer.html')
 
-		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
 
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
+    // $.getJSON('data/menu/index.json', function(menu_index){
+    //  console.log("got menu")
+    //  app.menu = menu_index
+    // })
+
+
+
+    var $window = $(window),
+        $body = $('body');
+
+    // Breakpoints.
+        breakpoints({
+            xlarge:  [ '1281px',  '1680px' ],
+            large:   [ '981px',   '1280px' ],
+            medium:  [ '737px',   '980px'  ],
+            small:   [ null,      '736px'  ]
+        });
+
+    // Play initial animations on page load.
+        $window.on('load', function() {
+            window.setTimeout(function() {
+                $body.removeClass('is-preload');
+            }, 100);
+        });
+
+
+
+    // Nav.
+
+        // Title Bar.
+            $(
+                '<div id="titleBar">' +
+                    '<a href="#navPanel" class="toggle"></a>' +
+                '</div>'
+            )
+                .appendTo($body);
+
+        // Panel.
+            $(
+                '<div id="navPanel">' +
+                    '<nav>' +
+                        $('#nav').navList() +
+                    '</nav>' +
+                '</div>'
+            )
+                .appendTo($body)
+                .panel({
+                    delay: 500,
+                    hideOnClick: true,
+                    hideOnSwipe: true,
+                    resetScroll: true,
+                    resetForms: true,
+                    side: 'left',
+                    target: $body,
+                    visibleClass: 'navPanel-visible'
+                });
+
 
 })(jQuery);
